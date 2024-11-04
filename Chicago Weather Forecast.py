0d6e1d64de7390efd9bcf2c1e1817affd556ba67
Bobby Perez <rjperez770@gmail.com>
2024-11-03 20:41:09 -0600

    added API key to code

--- Chicago Weather Forecast.py
+++ Chicago Weather Forecast.py
@@ -1,2 +1,11 @@
 import datetime as dt
 import requests
+
+BASE_URL = "http://api.openweathermap.org/data/3.0/weather?"
+API_KEY = "3b87eea11e1cdb5d9a919292b0a4f8bc"
+CITY = "Chicago"
+
+url = BASE_URL + "appid=" + API_KEY + "&q=" + CITY
+
+response = requests.get(url).json()
+

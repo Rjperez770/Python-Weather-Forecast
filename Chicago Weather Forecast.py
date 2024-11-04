236690acc8e2c5eae4a2bda76caece394709a363
Bobby Perez <rjperez770@gmail.com>
2024-11-04 14:55:20 -0600

    finished weather call

--- Chicago Weather Forecast.py
+++ Chicago Weather Forecast.py
@@ -10,4 +10,18 @@ url = BASE_URL + "appid=" + API_KEY + "&q=" + CITY + "&units=" + FAHRENHEIT
 
 response = requests.get(url).json()
 
-print(response)
+temp_f = response['main']['temp']
+feels_like_f = response['main']['feels_like']
+humidity = response['main']['humidity']
+description = response['weather'][0]['description']
+wind_speed = response['wind']['speed']
+sunrise_time = dt.datetime.utcfromtimestamp(response['sys']['sunrise'] + response['timezone'])
+sunset_time = dt.datetime.utcfromtimestamp(response['sys']['sunset'] + response['timezone'])
+
+print(f"Temperature in {CITY}: {temp_f}℉")
+print(f"Temperature in {CITY}: {feels_like_f}℉")
+print(f"Humidity in {CITY}: {humidity}%")
+print(f"Wind Speed in {CITY}: {wind_speed}m/s")
+print(f"General Weather in {CITY}: {description}")
+print(f"Sun Rises in {CITY} at {sunrise_time} local time")
+print(f"Sun Sets in {CITY} at {sunset_time} local time")

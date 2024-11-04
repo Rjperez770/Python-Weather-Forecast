b0feeaee5455e74b803b8bfd86f919c36763247f
Bobby Perez <rjperez770@gmail.com>
2024-11-04 14:37:11 -0600

    Working forecast in terminal

--- Chicago Weather Forecast.py
+++ Chicago Weather Forecast.py
@@ -1,11 +1,12 @@
 import datetime as dt
 import requests
 
-BASE_URL = "http://api.openweathermap.org/data/3.0/weather?"
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
 API_KEY = "3b87eea11e1cdb5d9a919292b0a4f8bc"
 CITY = "Chicago"
+FAHRENHEIT = "imperial"
 
-url = BASE_URL + "appid=" + API_KEY + "&q=" + CITY
+url = BASE_URL + "appid=" + API_KEY + "&q=" + CITY + "&units=" + FAHRENHEIT
 
 response = requests.get(url).json()
 
